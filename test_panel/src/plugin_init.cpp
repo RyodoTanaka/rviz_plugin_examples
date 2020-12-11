80e803d11f21d8105464059b36942a95a6d2c12e
RyodoTanaka <groadpg@gmail.com>
2020-12-11 11:23:37 +0000

    Delete unnecessary files

--- test_panel/src/plugin_init.cpp
+++
@@ -1,4 +0,0 @@
-#include <class_loader/class_loader.hpp>
-#include <rviz_plugin_tutorials/test_panel/test_panel.h>
-
-CLASS_LOADER_REGISTER_CLASS(rviz_plugin_tutorials::TestPanel, rviz_common::Panel)
