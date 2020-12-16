017ed7d3cc090e1d3c993050dd8501af4cce1f6c
RyodoTanaka <groadpg@gmail.com>
2020-12-16 19:59:23 +0900

    Update port to ROS1

--- include/rviz_plugin_examples/dial_panel.h
+++ include/rviz_plugin_examples/dial_panel.h
@@ -1,11 +1,10 @@
 #pragma once
 
 #ifndef Q_MOC_RUN
-#include <rclcpp/rclcpp.hpp>
+#include <ros/ros.h>
 #endif
 
-#include <rviz_common/panel.hpp>
-#include <std_msgs/msg/float64.hpp>
+#include <rviz/panel.h>
 
 namespace Ui {
 class DialUI;
@@ -13,7 +12,7 @@ class DialUI;
 
 namespace rviz_plugin_examples
 {
-class DialPanel: public rviz_common::Panel
+class DialPanel: public rviz::Panel
 {
   Q_OBJECT
  public:
@@ -34,7 +33,7 @@ protected:
   int value_{0};
   std::string topic_name_{"dial"};
 
-  rclcpp::Node::SharedPtr node_;
-  rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr pub_;
+  ros::NodeHandle nh_;
+  ros::Publisher pub_;
 };
 } // end namespace rviz_plugin_examples
--- src/dial_panel.cpp
+++ src/dial_panel.cpp
@@ -1,16 +1,13 @@
 #include <rviz_plugin_examples/dial_panel.h>
-#include <class_loader/class_loader.hpp>
+#include <pluginlib/class_list_macros.h>
+#include <std_msgs/Float64.h>
 
 #include "ui_dial_panel.h"
 
 namespace rviz_plugin_examples
 {
-static const rclcpp::Logger LOGGER = rclcpp::get_logger("rviz.plugin.examples.dial_panel");
-
-
 DialPanel::DialPanel(QWidget* parent) : Panel(parent),  ui_(new Ui::DialUI())
 {
-  node_ = std::make_shared<rclcpp::Node>("dial_panel", "rviz_plugin_examples");
   ui_->setupUi(this);
 }
 
@@ -25,7 +22,7 @@ void DialPanel::onInitialize()
 
   connect(ui_->line_edit,  SIGNAL(textChanged(const QString &)), this, SLOT(lineEditChanged()));
 
-  pub_ = node_->create_publisher<std_msgs::msg::Float64>(topic_name_, 1);
+  pub_ = nh_.advertise<std_msgs::Float64>("dial", 1);
   parentWidget()->setVisible(true);
 }
 
@@ -43,30 +40,33 @@ void DialPanel::onDisable()
 
 void DialPanel::lineEditChanged()
 {
+  std::string old_topic_name = topic_name_;
   if(ui_->line_edit->text().isEmpty())
-    ui_->line_edit->setText(QString::fromStdString(topic_name_));
+    topic_name_ = "dial";
   else
     topic_name_ = ui_->line_edit->text().toStdString();
 
-  pub_ = node_->create_publisher<std_msgs::msg::Float64>(topic_name_, 1);
-  RCLCPP_INFO(LOGGER, "You set the topic name : %s", topic_name_.c_str());
+  ROS_INFO("You set the topic name : %s", topic_name_.c_str());
+
+  if(old_topic_name != topic_name_)
+    pub_ = nh_.advertise<std_msgs::Float64>(topic_name_, 1);
 }
 
 void DialPanel::dialValueChanged(int value)
 {
   ui_->lcd->display(value);
   value_ = value;
-  RCLCPP_INFO(LOGGER, "You set the value : %d", value_);
+  ROS_INFO("You set the value : %d", value_);
 }
 
 void DialPanel::buttonClicked()
 {
-  std_msgs::msg::Float64 msg;
+  std_msgs::Float64 msg;
   msg.data = static_cast<double>(value_);
-  pub_->publish(msg);
-  RCLCPP_INFO(LOGGER, "You pushed the button.");
+  pub_.publish(msg);
+  ROS_INFO("You pushed the button.");
 }
 
 }  // namespace rviz_plugin_examples
 
-CLASS_LOADER_REGISTER_CLASS(rviz_plugin_examples::DialPanel, rviz_common::Panel)
+PLUGINLIB_EXPORT_CLASS(rviz_plugin_examples::DialPanel, rviz::Panel )
